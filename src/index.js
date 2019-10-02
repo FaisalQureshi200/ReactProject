a5f35f8748e3f1b1cbfdec0bf035db4b2a1b221d
faisalqureshi <faisalqureshi690@gmail.com>
2019-10-02 23:23:13 +0500

    This is a main file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import KeyPadButton from './button';
+import CalResult from './result';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css'
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            result:""
+        }
+    }
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "DEL"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    }
+    calculate = () => {
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(this.state.result) ) 
+            })
+        } catch (e) {
+            this.setState({
+                result: "syntax error"
+            })
+
+        }
+    }
+    reset=()=>{
+        this.setState({
+            result:" "
+        })
+
+    }
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0,-1)
+        })
+    };
+
+    render(){
+        return(
+            <div>
+                <h1 align="center">Simple Calculator</h1>
+                <div className="calculator-body">
+                    <CalResult result={this.state.result}/>
+                    <KeyPadButton onClick={this.onClick}/>
+                </div>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
